000a48682ef0ab97fb8e7c8fc63d387b826ec278
Yarong Mu <ymu@google.com>
2026-08-25 10:56:19 -0700

    scripts.

---
+++ scripts/vllm/benchmarking/bench_throughput_qwen_client.sh
@@ -0,0 +1,1 @@
+ENABLE_PALLAS_TP_MOE=1  python ./scripts/vllm/benchmarking/benchmark_serving.py --model Qwen/Qwen3.5-397B-A17B-FP8 --dataset-name random --backend vllm --random-input-len=1024 --random-output-len=8192 --num-prompts=2048 --random-range-ratio=0.8 --ignore-eos  --max-concurrency=512
